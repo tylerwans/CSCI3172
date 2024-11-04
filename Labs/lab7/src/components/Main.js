e5753e0cccd3fc22479b0af4ff01de96fb6e1208
Tyler <ty868750@dal.ca>
2024-11-03 23:37:23 -0400

    changes

--- Labs/lab7/src/components/Main.js
+++ Labs/lab7/src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import cat1Image from '../images/cat.jpg'; 
+import cat2Image from '../images/cat2.jpg'; 
 
 
 const MainContent = () => {
@@ -11,6 +12,11 @@ const MainContent = () => {
             <div>
             <h2>Look at this cat</h2>
             <img src={cat1Image} alt="Cat 1" style={{ width: '300px' }} />
+        </div>
+
+        <div>
+        <h2>This cat is frickn dunking!</h2>
+        <img src={cat2Image} alt="Cat 2" style={{ width: '300px' }} />
         </div>
             
         </main>
